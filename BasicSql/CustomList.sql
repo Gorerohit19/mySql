a81e2b86b71709d6a11cb684e4a6593d13cce87b
unknown <gore.rohit1902@gmail.com>
2022-12-03 17:52:18 +0530

    Customising List in mysql function

---
+++ BasicSql/CustomList.sql
@@ -0,0 +1,9 @@
+/*Field Function Example 1*/
+select field('A','A','B','C');
+/*Example 2*/
+select field('B','A','B','C');
+
+/* you can use the FIELD() function to map each order status to a number 
+and sort the result by the result of the FIELD() function:*/
+select orderNumber, status from orders
+order by 
