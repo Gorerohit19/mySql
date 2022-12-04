f0a3813a201703f6f27801c26c3b9b0e7a10a93c
unknown <gore.rohit1902@gmail.com>
2022-12-04 15:32:43 +0530

    Where clause with AND operator

--- BasicSql/WhereConditions.sql
+++ BasicSql/WhereConditions.sql
@@ -12,3 +12,8 @@ select lastName, firstName, jobTitle
 from employees
 where jobTitle = 'Sales Rep';
 
+/*The following example uses the WHERE clause to 
+find employees whose job titles are Sales Rep and office codes are 1:*/
+select lastName, firstName, jobTitle, officeCode
+from employees
+where jobtitle ='Sales Rep' and officeCode = 1;
