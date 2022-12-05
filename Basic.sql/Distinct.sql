625970c0473c249932e399ad02f0309bc18ad0b2
unknown <gore.rohit1902@gmail.com>
2022-12-05 19:52:00 +0530

    MySQL DISTINCT with multiple columns

--- Basic.sql/Distinct.sql
+++ Basic.sql/Distinct.sql
@@ -5,3 +5,14 @@ select lastName from employees order by lastName;
 
 /*select unique last names by adding the DISTINCT clause like this:*/
 select distinct lastName from employees order by lastName;
+
+/*When you use the DISTINCT clause to query the states, you will see distinct states and NULL as follows:*/
+select distinct state
+from customers;
+
+/*For example, to get a unique combination of city and state 
+from the customers table, you use the following query:*/
+select distinct state, city
+from customers
+where state is not null
+order by state, city;
