622f231c607030188ed02845045693538841151a
unknown <gore.rohit1902@gmail.com>
2022-12-06 16:07:16 +0530

    Showing 2 Country using OR operator

--- Basic.sql/OROperator.sql
+++ Basic.sql/OROperator.sql
@@ -10,3 +10,9 @@ select 0 or 0 ;
 /* When A and / or B is NULL, the OR operator returns 1 (true) if 
 either A or B is non-zero. Otherwise, it returns NULL. For example:*/
 select 1 or null, 0 or null, null or null; 
+
+/*The following query uses the OR operator in the WHERE clause to 
+select all the customers located in the USA or France:*/
+select customerName, country
+from customers
+where country='USA' or country='France';
